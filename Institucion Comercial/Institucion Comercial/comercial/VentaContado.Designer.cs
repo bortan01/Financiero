331f40e83f6c11b64bf8b9f7e22fe44d3f750cf8
Boris Ricardo Miranda <brmiranda009@gmail.com>
2018-12-21 21:46:07 -0600

    hay va

--- Institucion Comercial/Institucion Comercial/comercial/VentaContado.Designer.cs
+++ Institucion Comercial/Institucion Comercial/comercial/VentaContado.Designer.cs
@@ -31,31 +31,48 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.txtBuscarCliente = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.txtDuiEmpleado = new System.Windows.Forms.TextBox();
+            this.txtDuiCliente = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtNombreEmpleado = new System.Windows.Forms.TextBox();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.txtNitEmpleado = new System.Windows.Forms.TextBox();
+            this.txtNitCliente = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtTelefonoEmpleado = new System.Windows.Forms.TextBox();
+            this.txtTelefonoCliente = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.txtEmpleado = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.txtBuscarProducto = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.colCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodDisponible = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColDescripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(62, 226);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodigo,
+            this.CodDisponible,
+            this.ColPrecio,
+            this.ColDescripcion});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 166);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(737, 80);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // txtBuscarCliente
             // 
@@ -66,18 +83,18 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(458, 217);
+            this.textBox2.Location = new System.Drawing.Point(451, 366);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
             // 
-            // txtDuiEmpleado
+            // txtDuiCliente
             // 
-            this.txtDuiEmpleado.Enabled = false;
-            this.txtDuiEmpleado.Location = new System.Drawing.Point(243, 72);
-            this.txtDuiEmpleado.Name = "txtDuiEmpleado";
-            this.txtDuiEmpleado.Size = new System.Drawing.Size(147, 20);
-            this.txtDuiEmpleado.TabIndex = 3;
+            this.txtDuiCliente.Enabled = false;
+            this.txtDuiCliente.Location = new System.Drawing.Point(243, 72);
+            this.txtDuiCliente.Name = "txtDuiCliente";
+            this.txtDuiCliente.Size = new System.Drawing.Size(147, 20);
+            this.txtDuiCliente.TabIndex = 3;
             // 
             // label1
             // 
@@ -92,7 +109,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 170);
+            this.label2.Location = new System.Drawing.Point(109, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 5;
@@ -102,19 +119,19 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(204, 170);
+            this.label3.Location = new System.Drawing.Point(341, 363);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Cantidad";
             // 
-            // txtNombreEmpleado
+            // txtNombreCliente
             // 
-            this.txtNombreEmpleado.Enabled = false;
-            this.txtNombreEmpleado.Location = new System.Drawing.Point(61, 72);
-            this.txtNombreEmpleado.Name = "txtNombreEmpleado";
-            this.txtNombreEmpleado.Size = new System.Drawing.Size(147, 20);
-            this.txtNombreEmpleado.TabIndex = 7;
+            this.txtNombreCliente.Enabled = false;
+            this.txtNombreCliente.Location = new System.Drawing.Point(61, 72);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(147, 20);
+            this.txtNombreCliente.TabIndex = 7;
             // 
             // label4
             // 
@@ -136,18 +153,18 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(510, 167);
+            this.textBox5.Location = new System.Drawing.Point(630, 366);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 10;
             // 
-            // txtNitEmpleado
+            // txtNitCliente
             // 
-            this.txtNitEmpleado.Enabled = false;
-            this.txtNitEmpleado.Location = new System.Drawing.Point(422, 73);
-            this.txtNitEmpleado.Name = "txtNitEmpleado";
-            this.txtNitEmpleado.Size = new System.Drawing.Size(147, 20);
-            this.txtNitEmpleado.TabIndex = 11;
+            this.txtNitCliente.Enabled = false;
+            this.txtNitCliente.Location = new System.Drawing.Point(422, 73);
+            this.txtNitCliente.Name = "txtNitCliente";
+            this.txtNitCliente.Size = new System.Drawing.Size(147, 20);
+            this.txtNitCliente.TabIndex = 11;
             // 
             // label6
             // 
@@ -158,13 +175,13 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "Nit";
             // 
-            // txtTelefonoEmpleado
+            // txtTelefonoCliente
             // 
-            this.txtTelefonoEmpleado.Enabled = false;
-            this.txtTelefonoEmpleado.Location = new System.Drawing.Point(630, 76);
-            this.txtTelefonoEmpleado.Name = "txtTelefonoEmpleado";
-            this.txtTelefonoEmpleado.Size = new System.Drawing.Size(147, 20);
-            this.txtTelefonoEmpleado.TabIndex = 13;
+            this.txtTelefonoCliente.Enabled = false;
+            this.txtTelefonoCliente.Location = new System.Drawing.Point(630, 76);
+            this.txtTelefonoCliente.Name = "txtTelefonoCliente";
+            this.txtTelefonoCliente.Size = new System.Drawing.Size(147, 20);
+            this.txtTelefonoCliente.TabIndex = 13;
             // 
             // label7
             // 
@@ -202,26 +219,84 @@
             this.label8.TabIndex = 17;
             this.label8.Text = "Empleado";
             // 
+            // txtBuscarProducto
+            // 
+            this.txtBuscarProducto.Location = new System.Drawing.Point(201, 128);
+            this.txtBuscarProducto.Name = "txtBuscarProducto";
+            this.txtBuscarProducto.Size = new System.Drawing.Size(149, 20);
+            this.txtBuscarProducto.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 20);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(438, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(383, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Cantidad";
+            // 
+            // colCodigo
+            // 
+            this.colCodigo.HeaderText = "Código";
+            this.colCodigo.Name = "colCodigo";
+            // 
+            // CodDisponible
+            // 
+            this.CodDisponible.HeaderText = "Nombre";
+            this.CodDisponible.Name = "CodDisponible";
+            // 
+            // ColPrecio
+            // 
+            this.ColPrecio.HeaderText = "Precio";
+            this.ColPrecio.Name = "ColPrecio";
+            // 
+            // ColDescripcion
+            // 
+            this.ColDescripcion.HeaderText = "Descripción";
+            this.ColDescripcion.Name = "ColDescripcion";
+            this.ColDescripcion.Width = 433;
+            // 
             // VentaContado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBuscarProducto);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.txtEmpleado);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.txtTelefonoEmpleado);
+            this.Controls.Add(this.txtTelefonoCliente);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.txtNitEmpleado);
+            this.Controls.Add(this.txtNitCliente);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtNombreEmpleado);
+            this.Controls.Add(this.txtNombreCliente);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtDuiEmpleado);
+            this.Controls.Add(this.txtDuiCliente);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtBuscarCliente);
             this.Controls.Add(this.dataGridView1);
@@ -239,20 +314,28 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox txtBuscarCliente;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox txtDuiEmpleado;
+        private System.Windows.Forms.TextBox txtDuiCliente;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtNombreEmpleado;
+        private System.Windows.Forms.TextBox txtNombreCliente;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox txtNitEmpleado;
+        private System.Windows.Forms.TextBox txtNitCliente;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtTelefonoEmpleado;
+        private System.Windows.Forms.TextBox txtTelefonoCliente;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtEmpleado;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBuscarProducto;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodDisponible;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColPrecio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColDescripcion;
     }
 }
--- Institucion Comercial/Institucion Comercial/comercial/VentaContado.cs
+++ Institucion Comercial/Institucion Comercial/comercial/VentaContado.cs
@@ -16,7 +16,7 @@ namespace Institucion_Comercial.comercial
         {
             InitializeComponent();
         }
-
+        public String id_cliente ;
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -35,11 +35,15 @@ namespace Institucion_Comercial.comercial
                 try
                 {
                     DataSet ds;
-                    String sql = "select * from instituciones_financieras.cliente where nombre like ('%" + txtBuscarCliente.Text + "%')";
+                    String sql = "select * from instituciones_financieras.cliente where nombre like ('%" + txtBuscarCliente.Text + "%') or id_cliente like ('%" + txtBuscarCliente.Text + "%') ";
                     // MessageBox.Show(sql);
 
                     ds = Utilidades.Ejecutar(sql);
-                    dataGridView1.DataSource = ds.Tables[0];
+                    txtNombreCliente.Text = ds.Tables[0].Rows[0]["nombre"].ToString().Trim() + ds.Tables[0].Rows[0]["apellido"].ToString().Trim();
+                    txtDuiCliente.Text = ds.Tables[0].Rows[0]["dui"].ToString().Trim();
+                    txtNitCliente.Text = ds.Tables[0].Rows[0]["nit"].ToString().Trim();
+                    txtTelefonoCliente.Text = ds.Tables[0].Rows[0]["telefono"].ToString().Trim();
+                    id_cliente = ds.Tables[0].Rows[0]["id_cliente"].ToString().Trim();
                 }
                 catch (Exception error)
                 {
@@ -52,10 +56,39 @@ namespace Institucion_Comercial.comercial
         {
             String sql = "select id_empleado,nombre,apellido from instituciones_financieras.empleado where id_empleado =" +Login.codigo;
             DataSet ds;
-            MessageBox.Show(sql);
+            //MessageBox.Show(sql);
 
             ds = Utilidades.Ejecutar(sql);
             txtEmpleado.Text = ds.Tables[0].Rows[0]["nombre"].ToString().Trim() + ds.Tables[0].Rows[0]["apellido"].ToString().Trim();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtBuscarProducto.Text.Trim()) == false)
+            {
+                //MessageBox.Show("va por aqui");
+                try
+                {
+                    DataSet ds;
+                    String sql = "select id_producto,nombre,descripcion,precio_venta from instituciones_financieras.producto where nombre  like ('%" + txtBuscarProducto.Text + "%')   or id_producto like ('%" + txtBuscarProducto.Text + "%') ";
+                    // MessageBox.Show(sql);
+
+                    ds = Utilidades.Ejecutar(sql);
+                    dataGridView1.Rows.Add(ds.Tables[0].Rows[0]["id_producto"].ToString().Trim(),
+                                           ds.Tables[0].Rows[0]["nombre"].ToString().Trim(),
+                                           ds.Tables[0].Rows[0]["precio_venta"].ToString().Trim(),
+                                           ds.Tables[0].Rows[0]["descripcion"].ToString().Trim());
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("ha ocurrido un error " + error.Message);
+                }
+            }
         }
     }
 }
